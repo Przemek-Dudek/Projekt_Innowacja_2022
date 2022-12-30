47beff6fadb9c159be5a40789ba5e2bf9775f397
Jakub Szewczyk <240810@edu.p.lodz.pl>
2022-12-30 13:08:29 +0100

    dobry index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Dapp } from "./components/Dapp";
-import "./index.css";
+//import "./index.css";
 
 // We import bootstrap here, but you can remove if you want
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,8 +10,8 @@ import "bootstrap/dist/css/bootstrap.css";
 // react component. All of the logic is contained in it.
 
 ReactDOM.render(
-  <BrowseRouter>
+  <React.StrictMode>
     <Dapp />
-  </BrowseRouter>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
