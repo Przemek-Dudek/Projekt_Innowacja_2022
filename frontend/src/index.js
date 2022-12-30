2011057daff5df108243e792eb3df3f64d310a31
Jakub Szewczyk <240810@edu.p.lodz.pl>
2022-12-30 13:09:39 +0100

    commit na nowym branchu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import "bootstrap/dist/css/bootstrap.css";
 // This is the entry point of your application, but it just renders the Dapp
 // react component. All of the logic is contained in it.
 
+
 ReactDOM.render(
   <React.StrictMode>
     <Dapp />
