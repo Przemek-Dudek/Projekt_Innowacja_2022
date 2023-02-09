cd288487bf3e38dd1b140a9ff5a2cffcd8465fb6
Przemek Dudek <przemod77@gmail.com>
2023-02-09 15:43:07 +0100

    UTEST setup

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-
 // This is the main building block for smart contracts.
 contract Token is ERC20Capped, ERC20Burnable{
     // Some string type variables to identify the token.
@@ -22,5 +21,4 @@ contract Token is ERC20Capped, ERC20Burnable{
         require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
         super._mint(payable(account), amount);
     }
-
 }
