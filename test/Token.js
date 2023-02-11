8f3cf463877e61bd254d507cf1b345822c203911
Przemek Dudek <przemod77@gmail.com>
2023-02-11 14:28:07 +0100

    Pierwszy Test

--- test/Token.js
+++ test/Token.js
@@ -4,13 +4,31 @@ const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("ticketsDeploy", function () {
   async function deployTokenFixture() {
-    const ticketsDeploy = await ethers.getContractFactory("ticketsDeploy");
-    const [owner, addr1, addr2] = await ethers.getSigners(1000);
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const hardhatToken = await ticketsDeploy.deploy(1000);
+    const Ticket = await ethers.getContractFactory("ticketsDeploy");
+    const ticket = await Ticket.deploy();
+    await ticket.deployed();
 
-    await hardhatToken.deployed();
+    const DataBase = await ethers.getContractFactory("dataBase");
+    const dataBase = await DataBase.deploy("admin","admin2","malpa@wp.pl");
+    await dataBase.deployed();
+    
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy(ticket.address);
+    await token.deployed();
 
-    return { Token, hardhatToken, owner, addr1, addr2 };
+    const Market = await ethers.getContractFactory("Market");
+    const market = await Market.deploy();
+    await market.deployed();
+
+    return { owner, addr1, addr2, ticket, dataBase, token, market };
   }
+
+  it("Deploy contract and token and check values", async function(){
+    const { token } = await loadFixture(deployTokenFixture);
+    expect(await token.symbol()).to.equal("$TTPSC");
+    expect(await token.name()).to.equal("Transition Technologies");
+  });
+
 });
